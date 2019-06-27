7c484b41bc17ac2b18a236a14c59848322b1c3e6
Jason Lauman <jason@lauman.net>
2019-06-27 04:50:54 +0000

    update test and etl scripts

--- workspace/test.py
+++ workspace/test.py
@@ -20,42 +20,14 @@ spark = SparkSession \
     .getOrCreate()
 
 spark.newSession()
-
-# spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.access.key", config['S3']['AWS_ACCESS_KEY_ID'])
-# spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.secret.key", config['S3']['AWS_SECRET_ACCESS_KEY'])
-
-# spark._jsc.hadoopConfiguration().set("fs.s3a.awsAccessKeyId", config['S3']['AWS_ACCESS_KEY_ID'])
-# spark._jsc.hadoopConfiguration().set("fs.s3a.awsSecretAccessKey", config['S3']['AWS_SECRET_ACCESS_KEY'])
-
-# # see https://github.com/databricks/spark-redshift/issues/298#issuecomment-271834485
-# spark.sparkContext.setSystemProperty("com.amazonaws.services.s3.enableV4", "true")
-
-# # see https://stackoverflow.com/questions/28844631/how-to-set-hadoop-configuration-values-from-pyspark
-# hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
-
-# # # see https://stackoverflow.com/questions/43454117/how-do-you-use-s3a-with-spark-2-1-0-on-aws-us-east-2
-# hadoop_conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-# hadoop_conf.set("com.amazonaws.services.s3.enableV4", "true")
-# hadoop_conf.set("fs.s3a.access.key", config['S3']['AWS_ACCESS_KEY_ID'])
-# hadoop_conf.set("fs.s3a.secret.key", config['S3']['AWS_SECRET_ACCESS_KEY'])
-
-# # # see http://blog.encomiabile.it/2015/10/29/apache-spark-amazon-s3-and-apache-mesos/
-# # hadoop_conf.set("fs.s3a.connection.maximum", "100000")
-
-# # see https://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region
-# hadoop_conf.set("fs.s3a.endpoint", "s3." + 'us-east-2' + ".amazonaws.com")
-
-
 print(spark)
 
-# input_path = "s3a://jlauman-dend-project-04/log_data/*/*/*.json"
 input_path = "s3a://udacity-dend/log_data/*/*/*.json"
 df = spark.read.json(input_path)
 df.printSchema()
-
 
 os.environ['AWS_ACCESS_KEY_ID'] = config['S3']['AWS_ACCESS_KEY_ID']
 os.environ['AWS_SECRET_ACCESS_KEY'] = config['S3']['AWS_SECRET_ACCESS_KEY']
 
 
-df.write.csv('s3a://jlauman-dend-project-04-bucket2/log_data.csv')
+df.write.csv('s3a://jlauman-project-04/log_data.csv')
